39861e2c75889ab942162a605e9bb5dba5f17b10
Shoshana <mchjhho@gmail.com>
2021-05-25 14:32:44 +0300

    change to https

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,22 +4,19 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  baseUrl: string = "http://localhost:49238/api/";
   constructor(private http: HttpClient) { }
- //  key="zY5mgzGL376ZuxbfDGlhO4NhKOOfdNaZ";
-   //key="9GWwBEJiViCpuWXfRjb7w6yDuX4fec6d";
   key="DH6v2sA1Y3hI7KKkG9VSSNdNxAF1uft3";
    public getLocations(query: string): Observable<any> {
-    return this.http.get("http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+this.key+"&q="+query+"&language=en-us",{withCredentials: true},);
+    return this.http.get("https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+this.key+"&q="+query+"&language=en-us",{withCredentials: true},);
   }
 
   public getCurrentCondition(cityKey: number): Observable<any> {
-   return this.http.get("http://dataservice.accuweather.com/currentconditions/v1/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false",{withCredentials: true},);
+   return this.http.get("https://dataservice.accuweather.com/currentconditions/v1/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false",{withCredentials: true},);
   }
 
   public getForecast(cityKey: number): Observable<any> {
    
-    return this.http.get("http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false&metric=true",{withCredentials: true},);
+    return this.http.get("https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false&metric=true",{withCredentials: true},);
    
   }
 } 
