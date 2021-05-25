68aac1f443d1dd03fd6adbd883878a3377ce0286
Gittel Klein <gvolop@weel.com>
2021-05-25 20:55:50 +0300

    done

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,16 +7,16 @@ export class AppService {
   constructor(private http: HttpClient) { }
   key="DH6v2sA1Y3hI7KKkG9VSSNdNxAF1uft3";
    public getLocations(query: string): Observable<any> {
-    return this.http.get("https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+this.key+"&q="+query+"&language=en-us",{withCredentials: true},);
+    return this.http.get("https://yacdn.org/serve/https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+this.key+"&q="+query+"&language=en-us",{withCredentials: true},);
   }
 
   public getCurrentCondition(cityKey: number): Observable<any> {
-   return this.http.get("https://dataservice.accuweather.com/currentconditions/v1/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false",{withCredentials: true},);
+   return this.http.get("https://yacdn.org/serve/https://dataservice.accuweather.com/currentconditions/v1/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false",{withCredentials: true},);
   }
 
   public getForecast(cityKey: number): Observable<any> {
    
-    return this.http.get("https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false&metric=true",{withCredentials: true},);
+    return this.http.get("https://yacdn.org/serve/https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+cityKey+"?apikey="+this.key+"&language=en-us&details=false&metric=true",{withCredentials: true},);
    
   }
 } 
